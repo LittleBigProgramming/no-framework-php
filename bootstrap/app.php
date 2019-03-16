59344be9bb7fbd23803c958bfe3a1c36c9970eb4
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-03-16 19:49:46 +0000

    Added base container and environment variables configuration

---
+++ bootstrap/app.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+require_once  __DIR__ . '/../vendor/autoload.php';
+
+try {
+    $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../')->load();
+} catch (DotEnv\Exception\InvalidFileException $e) {
+    echo $e;
+}
+
+require_once __DIR__ . '/container.php';
