1e3984a1e78bf6d886cd808e1bc03a2ac6c252a0
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-03-18 16:45:42 +0000

    psr7 compliant view rendering - implemented methods to allow Twig templating system for use with the ViewServiceProvider using the View class which abstracts away the custom render method.

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -2,5 +2,10 @@
 
 $container = new League\Container\Container;
 
+$container->delegate(
+    new \League\Container\ReflectionContainer()
+);
+
 $container->addServiceProvider(new \App\Providers\AppServiceProvider());
+$container->addServiceProvider(new \App\Providers\ViewServiceProvider());
 
