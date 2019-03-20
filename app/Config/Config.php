76d4f84aa5e3431de642b53c18bb2d6eba8d95a0
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-03-20 10:53:17 +0000

    added config class with caching loading from .env files

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -2,7 +2,99 @@
 
 namespace App\Config;
 
+use App\Config\Loaders\Loader;
+
 class Config
 {
+    protected $config = [];
+    protected $cache = [];
+    /**
+     * @param array $loaders
+     * @return $this
+     */
+    public function load(array $loaders)
+    {
+        foreach ($loaders as $loader) {
+            if (!$loader instanceof Loader) {
+                continue;
+            }
 
+            $this->config = array_merge($this->config, $loader->parse());
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @return array|mixed
+     */
+    public function get($key, $default = null)
+    {
+        if ($this->existsInCache($key)) {
+            return $this->fromCache($key);
+        }
+
+        return $this->addToCache($key, $this->extractFromConfig($key) ?? $default);
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    protected function existsInCache($key)
+    {
+        return isset($this->cache[$key]);
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    protected function fromCache($key)
+    {
+        return $this->cache[$key];
+    }
+
+    /**
+     * @param $key
+     * @return array|mixed|void
+     */
+    protected function extractFromConfig($key)
+    {
+        $filtered = $this->config;
+
+        foreach (explode('.', $key) as $segment) {
+            if ($this->exists($filtered, $segment)) {
+                $filtered = $filtered[$segment];
+                continue;
+            }
+
+            return;
+        }
+
+        return $filtered;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return mixed
+     */
+    protected function addToCache($key, $value)
+    {
+        $this->cache[$key] = $value;
+
+        return $value;
+    }
+
+    /**
+     * @param array $config
+     * @param $key
+     * @return bool
+     */
+    protected function exists(array $config, $key)
+    {
+        return array_key_exists($key, $config);
+    }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,23 @@ try {
     echo $e;
 }
 
+$arrayLoader = new App\Config\Loaders\ArrayLoader([
+    'app' => base_path('config/app.php'),
+    'cache' => base_path('config/cache.php')
+]);
+
+$config = new App\Config\Config();
+$config->load([$arrayLoader]);
+
+dump($config->get('app.name.short'));
+
+die;
+
 require_once base_path('/bootstrap/container.php');
 
 $route = $container->get(\League\Route\RouteCollection::class);
 
-require_once base_path() . 'routes/web.php';
+require_once base_path('routes/web.php');
 
 $response = $route->dispatch(
     $container->get('request'), $container->get('response')
