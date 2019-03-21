fd385950a7c0321b7856cd8a82a3bdb46390ddcc
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-03-21 23:15:45 +0000

    added basic Auth routing, implemented base app template to derive from, added rules to ensure cached twig files aren't added to the repo

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 use App\Controllers\HomeController;
 
 $route->get('/', HomeController::class . '::index')->setName('home');
+
+$route->group('/auth', function ($route) {
+    $route->get('/login', 'App\Controllers\Auth\LoginController::index')->setName('auth.login');
+});
