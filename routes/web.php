a6a82870dcba81a9a0bbf51263b3882ad0597bf7
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-04-15 13:18:12 +0100

    create middleware to disable signed in users from registering and resigning in and rearrange route to remove auth prefix

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,19 @@
 use App\Controllers\DashboardController;
 use App\Controllers\HomeController;
 use App\Middleware\Authenticated;
+use App\Middleware\Guest;
 
 $route->get('/', HomeController::class . '::index')->setName('home');
 
 $route->group('', function ($route) {
     $route->get('/dashboard', DashboardController::class . '::index')->setName('dashboard');
+    $route->post('/logout', 'App\Controllers\Auth\LogoutController::logout')->setName('auth.logout');
 })->middleware($container->get(Authenticated::class));
 
-$route->group('/auth', function ($route) {
+$route->group('', function ($route) {
     $route->get('/login', 'App\Controllers\Auth\LoginController::index')->setName('auth.login');
     $route->post('/login', 'App\Controllers\Auth\LoginController::login');
-    $route->post('/logout', 'App\Controllers\Auth\LogoutController::logout')->setName('auth.logout');
-
 
     $route->get('/register', 'App\Controllers\Auth\RegistrationController::index')->setName('auth.register');
     $route->post('/register', 'App\Controllers\Auth\RegistrationController::register');
-});
+})->middleware($container->get(Guest::class));
