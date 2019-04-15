ba916bc54c3267807aefbade6d5aedf69cb6791d
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-04-15 13:03:58 +0100

    created Authentication middleware that is applied to a group so that if the user is not logged in then it will redirect to the home page

---
+++ app/Middleware/Authenticated.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Middleware;
+
+use App\Auth\Auth;
+
+class Authenticated
+{
+    protected $auth;
+
+    /**
+     * Authenticated constructor.
+     * @param Auth $auth
+     */
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * @param $request
+     * @param $response
+     * @param callable $next
+     * @return \Zend\Diactoros\Response\RedirectResponse
+     */
+    public function __invoke($request, $response, callable $next)
+    {
+        return !$this->auth->isLoggedIn() ? $response = redirect('/') : $next($request, $response);
+    }
+}
